106f26447a03a98dfd290d67e37816e33e79002f
integrate <vmatva@gmail.com>
2021-01-25 18:17:01 +0300

    time check added to init.py package name changed to wrap_py due to conflict on PyPI

--- wrap_engine/__init__.py
+++ wrap_engine/__init__.py
@@ -1,3 +1,11 @@
+from datetime import timedelta, datetime
+
+now = datetime.now()
+last_allowed_day = datetime(2021, 3, 1)
+period = last_allowed_day - now
+if period.days<0:
+    print("Library expired!")
+    exit()
 
 
 from wrap_engine import wrap_world as world, wrap_app as app, wrap_event as event

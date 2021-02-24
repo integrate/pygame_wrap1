b0b4da6c33c3c2a670248d8f79ab850c8561c9d8
integrate <vmatva@gmail.com>
2021-02-24 12:10:19 +0300

    twine upload script changed to respect win32 version

--- scripts/upload.bat
+++ scripts/upload.bat
@@ -2,6 +2,8 @@ set base_dir=%~dp0..
 set ver=0.1.3
 
 set twine=%base_dir%\venv\scripts\twine.exe
-set ver_name=%base_dir%\dist\wrap_engine-%ver%-cp38-cp38-win_amd64.whl
+set ver_name32=%base_dir%\dist\wrap_engine-%ver%-cp38-cp38-win32.whl
+set ver_name64=%base_dir%\dist\wrap_engine-%ver%-cp38-cp38-win_amd64.whl
 
-%twine% upload %ver_name%
+IF EXIST %ver_name32% (%twine% upload %ver_name32%)
+IF EXIST %ver_name64% (%twine% upload %ver_name64%)

c3bb51f307f2cbad342574078fa3407efc38abbd
integrate <vmatva@gmail.com>
2021-04-23 00:53:13 +0300

    bug with sprite.get_width_pix fixed version changed to 0.1.8 date changed to 20210901

--- scripts/upload.bat
+++ scripts/upload.bat
@@ -1,5 +1,5 @@
 set base_dir=%~dp0..
-set ver=0.1.7.1.20210601
+set ver=0.1.8.1.20210901
 
 set twine=%base_dir%\venv\scripts\twine.exe
 set ver_name32=%base_dir%\dist\wrap_engine-%ver%-cp38-cp38-win32.whl

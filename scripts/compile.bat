5d29e1cb45eaf2ef5bc1a0940339d3de397fa2bf
integrate <vmatva@gmail.com>
2021-08-12 14:39:59 +0300

    tox integration

--- scripts/compile.bat
+++
@@ -1,8 +0,0 @@
-set base_dir=%~dp0..
-
-if exist %base_dir%\build (echo y | rmdir /s %base_dir%\build)
-if exist %base_dir%\cbuild_temp (echo y | rmdir /s %base_dir%\cbuild_temp)
-if exist %base_dir%\dist (echo y | rmdir /s %base_dir%\dist)
-if exist %base_dir%\wrap_engine.egg-info (echo y | rmdir /s %base_dir%\wrap_engine.egg-info)
-
-%base_dir%\setup.py cbuild
--- scripts/upload.bat
+++
@@ -1,9 +0,0 @@
-set base_dir=%~dp0..
-set ver=0.2.3.1.20210901
-
-set twine=%base_dir%\venv\scripts\twine.exe
-set ver_name32=%base_dir%\dist\wrap_engine-%ver%-cp38-cp38-win32.whl
-set ver_name64=%base_dir%\dist\wrap_engine-%ver%-cp38-cp38-win_amd64.whl
-
-IF EXIST %ver_name32% (%twine% upload %ver_name32%)
-IF EXIST %ver_name64% (%twine% upload %ver_name64%)

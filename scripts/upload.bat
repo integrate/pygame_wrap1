e6ff3a7e73eeab998763347265b9bd77af78823f
Vyacheslav Matva <vmatva@gmail.com>
2026-09-18 03:00:33 +0300

    New version of pygame and python

--- scripts/upload.bat
+++ scripts/upload.bat
@@ -1,9 +1,7 @@
 set base_dir=%~dp0..
 set ver=0.2.5.1.21220701
 
-set twine=%base_dir%\venv\scripts\twine.exe
-set ver_name32=%base_dir%\dist\wrap_engine-%ver%-cp313-cp313-win32.whl
-set ver_name64=%base_dir%\dist\wrap_engine-%ver%-cp313-cp313-win_amd64.whl
+set twine=%base_dir%\.venv\scripts\twine.exe
+set ver_name=%base_dir%\dist\wrap_engine-0.2.5.1.21220701-py3-none-any.whl
 
-IF EXIST %ver_name32% (%twine% upload %ver_name32%)
-IF EXIST %ver_name64% (%twine% upload %ver_name64%)
+IF EXIST %ver_name% (%twine% upload %ver_name%)
